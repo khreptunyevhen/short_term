06e3d49d719e6afb342973f122ba996c39fbc851
Yevhen Khreptun <khreptun.e@gmail.com>
2024-08-07 16:23:33 -0400

    restructure the explore_data

--- code/explore_data.py
+++ code/explore_data.py
@@ -1,54 +1,63 @@
 import pandas as pd
 from helpers import merge_with_selected_columns, define_building, define_cross, calculate_cleaning
-from variables import DATE_FOR_REPORT, EXTERNAL_SOURCE_FILES
+from variables import DATE_FOR_REPORT, EXTERNAL_SOURCE_FILES, TAXES
 
-def create_ota_marketing_report(reservations_df, transactions_df):
-    # Load data
+COLUMN_ORDER = ["Accommodation Total", "Amount Paid", "balance_due", "lodging_tax", "gst", "qst", "Grand Total", "Debit", "Check in Date", "Check out Date", "Nights", "Reservation_id", "Third Party Confirmation Number", "Name", "Room Number", "building", "Source", "Cleaning_+20%", "booking_marketing", "expedia_marketing", "airbnb_marketing", "airbnb_paid", "airbnb_total", "cross", "notes"]
+
+def load_ota_data():
+    """Load data from external sources (Expedia, Booking, Airbnb)."""
     expedia_df = pd.read_excel(EXTERNAL_SOURCE_FILES["expedia"])
     booking_df = pd.read_excel(EXTERNAL_SOURCE_FILES["booking"])
     airbnb_df = pd.read_csv(EXTERNAL_SOURCE_FILES["airbnb"])
 
-    airbnb_df_filtered = airbnb_df[airbnb_df["Type"] != "Payout"]
+    return (expedia_df, booking_df, airbnb_df)
 
-    merge_col = "Reservation_id"
+def merge_reservation_data(ota_reservations, cb_reservations, cb_transactions):
+    """Merge OTA and Cloudbeds reservation data with relevant columns."""
+    expedia_df, booking_df, airbnb_df = ota_reservations
 
-    # Change type for the reservations ID
+    # Ensure reservation ID columns are strings
     booking_df["Reservation number"] = booking_df["Reservation number"].astype(str)
     expedia_df["Reservation ID"] = expedia_df["Reservation ID"].astype(str)
+    cb_reservations["Third Party Confirmation Number"] = cb_reservations["Third Party Confirmation Number"].astype(str)
 
-    # Merge data
-    df_with_debit = merge_with_selected_columns(reservations_df, transactions_df, merge_col, "Reservation Number", "Res #")
-    merged_with_booking_df = merge_with_selected_columns(df_with_debit, booking_df, "Third Party Confirmation Number", "Third Party Confirmation Number", "Reservation number", ["Commission amount"])
-    merged_with_expedia_df = merge_with_selected_columns(merged_with_booking_df, expedia_df, "Third Party Confirmation Number", "Third Party Confirmation Number", "Reservation ID", ["Total Amount Due"])
-    merged_with_airbnb_df = merge_with_selected_columns(merged_with_expedia_df, airbnb_df_filtered, "Third Party Confirmation Number", "Third Party Confirmation Number", "Confirmation Code", ["Amount", "Service fee", "Gross earnings"])
+    airbnb_df_filtered = airbnb_df[airbnb_df["Type"] != "Payout"]
 
+    # Merge dataframes
+    reservations_with_debit = merge_with_selected_columns(cb_reservations, cb_transactions, "Reservation_id", "Reservation Number", "Res #")
+    merged_with_booking = merge_with_selected_columns(reservations_with_debit, booking_df, "Third Party Confirmation Number", "Third Party Confirmation Number", "Reservation number", ["Commission amount"])
+    merged_with_expedia = merge_with_selected_columns(merged_with_booking, expedia_df, "Third Party Confirmation Number", "Third Party Confirmation Number", "Reservation ID", ["Total Amount Due"])
+    merged_with_airbnb = merge_with_selected_columns(merged_with_expedia, airbnb_df_filtered, "Third Party Confirmation Number", "Third Party Confirmation Number", "Confirmation Code", ["Amount", "Service fee", "Gross earnings"])
+
+    return merged_with_airbnb
+
+def add_additional_columns(data):
+    """Add calculated columns to the merged reservation data."""
     # Rename columns
-    merged_with_airbnb_df.rename(columns={"Amount" : "airbnb_paid", "Service fee" : "airbnb_commission", "Gross earnings" : "airbnb_total", "Total Amount Due" : "expedia_marketing", "Commission amount" : "booking_marketing"}, inplace=True, errors="ignore")
-
-    merged_with_airbnb_df["notes"] = ""
+    data.rename(columns={"Amount" : "airbnb_paid", "Service fee" : "airbnb_marketing", "Gross earnings" : "airbnb_total", "Total Amount Due" : "expedia_marketing", "Commission amount" : "booking_marketing"}, inplace=True, errors="ignore")
 
     # Calculate taxes
-    merged_with_airbnb_df["lodging_tax"] = round(merged_with_airbnb_df.apply(lambda row: row["Accommodation Total"] * 0.035 if row["Source"] != "Airbnb (API)" else 0, axis=1), 2)
-    merged_with_airbnb_df["gst"] = round(merged_with_airbnb_df.apply(lambda row: (row["lodging_tax"] + row["Accommodation Total"]) * 0.05 if row["Source"] != "Airbnb (API)" else 0, axis=1), 2)
-    merged_with_airbnb_df["qst"] = round(merged_with_airbnb_df.apply(lambda row: (row["lodging_tax"] + row["Accommodation Total"]) * 0.09975 if row["Source"] != "Airbnb (API)" else 0, axis=1), 2)
+    data["lodging_tax"] = round(data.apply(lambda row: row["Accommodation Total"] * TAXES["lodging_tax"] if row["Source"] != "Airbnb (API)" else 0, axis=1), 2)
+    data["gst"] = round(data.apply(lambda row: (row["lodging_tax"] + row["Accommodation Total"]) * TAXES["gst"] if row["Source"] != "Airbnb (API)" else 0, axis=1), 2)
+    data["qst"] = round(data.apply(lambda row: (row["lodging_tax"] + row["Accommodation Total"]) * TAXES["qst"] if row["Source"] != "Airbnb (API)" else 0, axis=1), 2)
 
-    merged_with_airbnb_df["building"] = merged_with_airbnb_df.apply(define_building, axis=1)
+    # Add additional columns
+    data["balance_due"] = round(data["Accommodation Total"] + data["lodging_tax"] + data["gst"] + data["qst"] - data["Grand Total"], 2)
+    data["building"] = data.apply(define_building, axis=1)
+    data["cross"] = data.apply(define_cross, axis=1, args=(DATE_FOR_REPORT["month"],))
+    data["Cleaning_+20%"] = data.apply(calculate_cleaning, axis=1)
+    data["notes"] = ""
 
-    # Calculate balance due
-    merged_with_airbnb_df["balance_due"] = round(merged_with_airbnb_df["Accommodation Total"] + merged_with_airbnb_df["lodging_tax"] + merged_with_airbnb_df["gst"] + merged_with_airbnb_df["qst"] - merged_with_airbnb_df["Grand Total"], 2)
+    return data
 
-    merged_with_airbnb_df["cross"] = merged_with_airbnb_df.apply(define_cross, axis=1, args=(DATE_FOR_REPORT["month"],))
+def reorder_columns(data, order):
+    """Reorder columns in the DataFrame to match the desired order."""
+    return data.reindex(columns=order)
 
-    merged_with_airbnb_df["Cleaning_+20%"] = merged_with_airbnb_df.apply(calculate_cleaning, axis=1)
+def process_reservations(current_reservations, cb_sum_debit):
+    reservations_dfs = list(load_ota_data())
+    merged_reservations = merge_reservation_data(reservations_dfs, current_reservations, cb_sum_debit)
+    reservations_with_calculations = add_additional_columns(merged_reservations)
+    final_reservation = reorder_columns(reservations_with_calculations, COLUMN_ORDER)
 
-    merged_with_airbnb_df = merged_with_airbnb_df.drop(columns=["Lodging Tax", "Good and Services Tax", "Quebec Sales Tax", "check_in_month", "check_out_month", "Status", "Cancelation fee"])
-    merged_with_airbnb_df["Third Party Confirmation Number"] = merged_with_airbnb_df["Third Party Confirmation Number"].astype(str)
-
-    # Desired column order
-    desired_order = ["Accommodation Total", "Amount Paid", "balance_due", "lodging_tax", "gst", "qst", "Grand Total", "Debit", "Check in Date", "Check out Date", "Nights", "Reservation_id", "Third Party Confirmation Number", "Name", "Room Number", "building", "Source", "Cleaning_+20%", "booking_marketing", "expedia_marketing", "airbnb_paid", "airbnb_commission", "airbnb_total", "cross", "notes"]
-
-    merged_with_airbnb_df = merged_with_airbnb_df.reindex(columns=desired_order)
-
-    merged_with_airbnb_df["Reservation_id"] = merged_with_airbnb_df["Reservation_id"].astype(int)
-
-    return merged_with_airbnb_df
+    return final_reservation
--- code/main.py
+++ code/main.py
@@ -1,16 +1,18 @@
 from reservations import current_reservations, no_show_reservations, canceled_reservations
 from transactions import cb_sum_debit
-from explore_data import create_ota_marketing_report
+from explore_data import process_reservations
 from helpers import import_to_excel
 from variables import FINAL_FOLDER_LINK, FINAL_FILES_NAME
 
 def main():
+    final_reservations = process_reservations(current_reservations, cb_sum_debit)
+    
     import_to_excel(current_reservations, FINAL_FILES_NAME["current_reservations"], FINAL_FOLDER_LINK)
     import_to_excel(no_show_reservations, FINAL_FILES_NAME["no_show_reservations"], FINAL_FOLDER_LINK)
     import_to_excel(canceled_reservations, FINAL_FILES_NAME["canceled_reservations"], FINAL_FOLDER_LINK)
     import_to_excel(cb_sum_debit, FINAL_FILES_NAME["cb_debit_summary"], FINAL_FOLDER_LINK)
 
-    import_to_excel(create_ota_marketing_report(current_reservations, cb_sum_debit), FINAL_FILES_NAME["combined_for_analysis"], FINAL_FOLDER_LINK)
+    import_to_excel(final_reservations, FINAL_FILES_NAME["combined_for_analysis"], FINAL_FOLDER_LINK)
 
 if __name__ == "__main__":
     main()
