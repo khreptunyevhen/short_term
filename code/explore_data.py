61cccac9ac7976550a59b464e739f1b061cb50b2
Yevhen Khreptun <khreptun.e@gmail.com>
2024-08-28 13:37:13 -0400

    add docstrings

--- code/explore_data.py
+++ code/explore_data.py
@@ -3,6 +3,25 @@ from combined_reservations import merged_reservations
 from settings.constants import TAXES, DATE_FOR_REPORT, COLUMN_ORDER
 
 def add_additional_columns(data):
+    """
+    Add additional calculated columns to the DataFrame.
+
+    This function calculates and adds the following columns to the DataFrame:
+    - "Lodging Tax": Computed as a percentage of "Accommodation Total", except for rows where the source is "Airbnb (API)".
+    - "GST": Calculated as a percentage of the sum of "Lodging Tax" and "Accommodation Total", except for rows where the source is "Airbnb (API)".
+    - "QST": Calculated as a percentage of the sum of "Lodging Tax" and "Accommodation Total", except for rows where the source is "Airbnb (API)".
+    - "Balance Due": The difference between the total amount due and the grand total.
+    - "Building": Determined by the `define_building` function.
+    - "Cross": Determined by the `define_cross` function, with the current month passed as an argument.
+    - "Cleaning": Calculated by the `calculate_cleaning` function.
+    - "Total marketing (no Airbnb)": The sum of marketing amounts from various sources excluding Airbnb.
+
+    Parameters:
+    - data (pd.DataFrame): The DataFrame containing the original reservation data.
+
+    Returns:
+    - pd.DataFrame: The DataFrame with additional columns added.
+    """
 
     # Calculate taxes
     data["Lodging Tax"] = round(data.apply(lambda row: row["Accommodation Total"] * TAXES["lodging_tax"] if row["Source"] != "Airbnb (API)" else 0, axis=1), 2)
@@ -19,10 +38,31 @@ def add_additional_columns(data):
     return data
 
 def reorder_columns(data, order):
-    """Reorder columns in the DataFrame to match the desired order."""
+    """
+    Reorder columns in the DataFrame to match the desired order.
+
+    This function rearranges the columns of the DataFrame according to the specified order list.
+
+    Parameters:
+    - data (pd.DataFrame): The DataFrame with columns to be reordered.
+    - order (list of str): The desired column order.
+
+    Returns:
+    - pd.DataFrame: The DataFrame with columns reordered as specified.
+    """
+
     return data.reindex(columns=order)
 
 def create_final_reservations():
+    """
+    Create the final reservations DataFrame by adding additional columns and reordering columns.
+
+    This function processes the reservations data by first adding calculated columns and then reordering the columns based on the predefined order.
+
+    Returns:
+    - pd.DataFrame: The final reservations DataFrame with additional and reordered columns.
+    """
+    
     reservations_df = add_additional_columns(merged_reservations)
     reservations_df = reorder_columns(reservations_df, COLUMN_ORDER)
 
